19c8e349607ae39b0e1af78e97fc79101e519ea1
Travis CI User <travis@example.org>
2017-10-22 12:52:30 +0000

    Documentation for alkis/decimal@5250357

---
+++ implementors/core/convert/trait.AsRef.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["decimal"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"decimal/struct.d128.html\" title=\"struct decimal::d128\">d128</a>&gt; for <a class=\"struct\" href=\"decimal/struct.d128.html\" title=\"struct decimal::d128\">d128</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["decimal"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"decimal/struct.d128.html\" title=\"struct decimal::d128\">d128</a>",];
+implementors["serde"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"serde/de/struct.IgnoredAny.html\" title=\"struct serde::de::IgnoredAny\">IgnoredAny</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
